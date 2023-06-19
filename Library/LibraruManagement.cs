8f021aeec0b72464d8c16b95cfefcc113580e7ab
Bahriddin1234 <bahriddinabdusalomov07@gmail.com>
2023-06-19 18:54:27 +0500

    Created library management model

---
+++ Library/LibraruManagement.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class LibraruManagement : LibraryManagementCore
+    {
+        public void RegisterUser(string firstName, string lastName)
+        {
+
+        }
+
+        public void RegisterUser(string firstName, string lastName, string userName)
+        {
+            var foundUser = false;
+
+            foreach(var user in Store.Users)
+            {
+                if(user.UserName == userName)
+                {
+                    foundUser = true;
+                    break;
+                }
+
+                if (foundUser)
+                    return;
+
+                Store.Users.Add(new User(firstName, lastName, userName));
+            }
+        }
+
+
+
+
+    }
+}
