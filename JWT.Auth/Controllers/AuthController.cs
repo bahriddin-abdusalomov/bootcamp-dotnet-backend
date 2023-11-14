f09d3b3c5caa20084faad65d15f8566257a9f7ad
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-14 20:13:24 +0500

    created controller

---
+++ JWT.Auth/Controllers/AuthController.cs
@@ -0,0 +1,38 @@
+﻿using JWT.Auth.Dtos;
+using JWT.Auth.Services.Auth;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthService _authService;
+        private readonly ILogger<AuthController> _logger;
+
+        public AuthController(IAuthService authService, ILogger<AuthController> logger)
+        {
+            _authService = authService;
+            _logger = logger;
+
+        }
+
+        [HttpPost]
+        public async ValueTask<IActionResult> Login(UserDto request)
+        {
+            try
+            {
+                var token = await _authService.Login(request);
+
+                return Ok(token);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message, ex.StackTrace);
+                return BadRequest("Username or Password is not valid");
+            }
+        }
+    }
+}
---
+++ JWT.Auth/Controllers/UsersController.cs
@@ -0,0 +1,29 @@
+﻿using JWT.Auth.Data;
+using JWT.Auth.Dtos;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace JWT.Auth.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private readonly AppDbContext _dbContext;
+
+        public UsersController(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [Authorize]
+        [HttpGet(Name = "Users")]
+        public async ValueTask<IActionResult> GetAllUserAsync(UserDto userDto)
+        {
+            var users = await _dbContext.Users.ToListAsync();
+            return Ok(users);
+        }
+    }
+}
