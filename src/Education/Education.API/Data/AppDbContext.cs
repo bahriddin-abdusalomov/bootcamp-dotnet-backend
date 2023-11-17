2fbd1146f80f52b9f52d0de1dbfa7e78077a297b
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-17 21:30:03 +0500

    created data context folder

---
+++ src/Education/Education.API/Data/AppDbContext.cs
@@ -0,0 +1,24 @@
+﻿using Education.API.Configurations;
+using Education.API.Models.Students;
+using Education.API.Models.Teachers;
+using Education.API.Models.Users;
+using Microsoft.EntityFrameworkCore;
+
+namespace Education.API.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {}
+
+    public DbSet<User> Users { get; set; }
+    public DbSet<Student> Students { get; set; }
+    public DbSet<Teacher> Teachers { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new UsersConfiguration());
+        modelBuilder.ApplyConfiguration(new StudentsConfiguration());
+        modelBuilder.ApplyConfiguration(new TeacherConfiguration());
+    }
+}
