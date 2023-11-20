e9ad0d5d80a3fb6549960c089013a01ebdce07dc
bahriddin-abdusalomov <bahriddinabdusalomov07@gmail.com>
2023-11-20 14:41:26 +0500

    created user model

---
+++ RedisCache/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace RedisCache.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
